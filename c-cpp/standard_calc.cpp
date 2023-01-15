6f58f0aed2a2e4602f32d4ef2120ba3e1e046aee
Devon Friend <69879126+DFriend01@users.noreply.github.com>
2023-01-14 16:03:43 -0800

    Add examples to coding questions for more clarity (#3)
    * Add examples to coding questions for more clarity
    
    * Add .0 to examples for float return values

--- c-cpp/standard_calc.cpp
+++ c-cpp/standard_calc.cpp
@@ -4,6 +4,10 @@
 /**
  * @brief Bounds the provided angle between [-180, 180) degrees.
  *
+ * e.g.)
+ *      bound_to_180(135) = 135.0
+ *      bound_to_180(200) = -160.0
+ *
  * @param angle: The input angle in degrees.
  *
  * @return float: The bounded angle in degrees.
@@ -14,6 +18,10 @@ float bound_to_180(float angle) {
 
 /**
  * @brief Determines whether an angle is between two other angles
+ *
+ *  e.g.)
+ *      is_angle_between(0, 45, 90) = true
+ *      is_angle_between(45, 90, 270) = false
  * 
  * @param first_angle:  The first bounding angle in degrees.
  * @param middle_angle: The angle in question in degrees.
